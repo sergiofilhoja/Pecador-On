3f60f63fda642ec25773a45f04f25e4f8d63a3eb
Sérgio Filho <sergiofilho@MacBook-Pro-de-Sergio.local>
2022-04-14 14:54:05 -0300

    Adicao de dados

---
+++ src/utils/missas.js
@@ -0,0 +1,769 @@
+export default missas = [
+    // Segunda
+    { 
+        id: 1,
+        name: 'Seminário Arquidiocesano da Paraíba',
+        zona: 'Leste | Praia',
+        bairro: 'Castelo Branco',
+        horarios: ['7:00'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 2,
+        name: 'Comunidade Mel de Deus',
+        zona: 'Leste | Praia',
+        bairro: 'Cabo Branco',
+        horarios: ['7:30'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 3,
+        name: 'Paróq. S. Antônio de Lisboa',
+        zona: 'Leste | Praia',
+        bairro: 'Tambaú',
+        horarios: ['17:00'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 4,
+        name: 'Paróq. S. Pedro Pescador',
+        zona: 'Leste | Praia',
+        bairro: 'Manaíra',
+        horarios: ['17:00'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 5,
+        name: 'Paróq. S. Pedro S. Paulo',
+        zona: 'Leste | Praia',
+        bairro: 'Brisamar',
+        horarios: ['17:00'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 6,
+        name: 'Paróq. N. S. de Guadalupe',
+        zona: 'Leste | Praia',
+        bairro: 'Cabo Branco',
+        horarios: ['17:00'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 7,
+        name: 'Paróq. N. S. Auxiliadora dos Cristãos',
+        zona: 'Leste | Praia',
+        bairro: 'Bessa',
+        horarios: ['17:00'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 8,
+        name: 'Paróq. N. S. de Nazaré',
+        zona: 'Leste | Praia',
+        bairro: 'Jd. Oceania',
+        horarios: ['17:30'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 9,
+        name: 'Paróq. S. Pedro S. Paulo',
+        zona: 'Leste | Praia',
+        bairro: 'Brisamar',
+        horarios: ['17:00'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 10,
+        name: 'Catedral N. S. das Neves',
+        zona: 'Norte',
+        bairro: 'Centro',
+        horarios: ['12:00'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 11,
+        name: 'Paróq. S. Júlia',
+        zona: 'Norte',
+        bairro: 'Torre',
+        horarios: ['17:00'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 12,
+        name: 'Paróq. S. Inácio de Loyola',
+        zona: 'Norte',
+        bairro: 'Mandacaru',
+        horarios: ['18:30'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 13,
+        name: 'Igreja São Gonçalo',
+        zona: 'Norte',
+        bairro: 'Torre',
+        horarios: ['19:00'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 14,
+        name: 'Paróq. Jesus Cristo Rei',
+        zona: 'Sul',
+        bairro: 'Mangabeira',
+        horarios: ['16:30'],
+        dias: ['Segunda']
+    },
+    { 
+        id: 15,
+        name: 'Paróq. de Aparecida',
+        zona: 'Sul',
+        bairro: 'Valentina',
+        horarios: ['19:00'],
+        dias: ['Segunda']
+    },
+
+
+    // Terça
+    { 
+        id: 16,
+        name: 'Sant. Imaculada Conceição',
+        zona: 'Leste | Praia',
+        bairro: 'Tambauzinho',
+        horarios: ['6:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 17,
+        name: 'Com. Doce Mãe de Deus',
+        zona: 'Leste | Praia',
+        bairro: 'Bessa',
+        horarios: ['7:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 18,
+        name: 'Com. Fraterno Amor - Oásis',
+        zona: 'Leste | Praia',
+        bairro: 'Camboinha III',
+        horarios: ['8:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 19,
+        name: 'Seminário Arquidiocesano da Paraíba',
+        zona: 'Leste | Praia',
+        bairro: 'Castelo Branco',
+        horarios: ['7:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 20,
+        name: 'Comunidade Mel de Deus',
+        zona: 'Leste | Praia',
+        bairro: 'Cabo Branco',
+        horarios: ['7:30'],
+        dias: ['Terça']
+    },
+    { 
+        id: 21,
+        name: 'Paróq. S. Antônio de Lisboa',
+        zona: 'Leste | Praia',
+        bairro: 'Tambaú',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 22,
+        name: 'Paróq. S. Pedro Pescador',
+        zona: 'Leste | Praia',
+        bairro: 'Manaíra',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 23,
+        name: 'Paróq. S. Pedro S. Paulo',
+        zona: 'Leste | Praia',
+        bairro: 'Brisamar',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 24,
+        name: 'Paróq. N. S. de Guadalupe',
+        zona: 'Leste | Praia',
+        bairro: 'Cabo Branco',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 25,
+        name: 'Paróq. São Miguel',
+        zona: 'Leste | Praia',
+        bairro: 'Bessa',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 26,
+        name: 'Paróq. N. S. de Fátima',
+        zona: 'Leste | Praia',
+        bairro: 'Mirarmar',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 27,
+        name: 'Paróq. N. S. Auxiliadora dos Cristãos',
+        zona: 'Leste | Praia',
+        bairro: 'Bessa',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 28,
+        name: 'Paróq. N. S. do Perpétuo Socorro',
+        zona: 'Leste | Praia',
+        bairro: 'Altiplano',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 29,
+        name: 'Sant. Imaculada Conceição',
+        zona: 'Leste | Praia',
+        bairro: 'Tambauzinho',
+        horarios: ['17:15'],
+        dias: ['Terça']
+    },
+    { 
+        id: 30,
+        name: 'Paróq. N. S. de Nazaré',
+        zona: 'Leste | Praia',
+        bairro: 'Jd Oceania',
+        horarios: ['17:30'],
+        dias: ['Terça']
+    },
+    { 
+        id: 31,
+        name: 'Paróq. Maria Mãe de Deus',
+        zona: 'Leste | Praia',
+        bairro: 'Intermares',
+        horarios: ['19:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 32,
+        name: 'Paróq. N. S. de Nazaré',
+        zona: 'Leste | Praia',
+        bairro: 'Poço',
+        horarios: ['19:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 33,
+        name: 'Paróq. São Rafael',
+        zona: 'Leste | Praia',
+        bairro: 'Castelo Branco',
+        horarios: ['19:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 34,
+        name: 'Sant. Mãe Rainha',
+        zona: 'Leste | Praia',
+        bairro: 'Bessa',
+        horarios: ['19:30'],
+        dias: ['Terça']
+    },
+    { 
+        id: 35,
+        name: 'Catedral N. S. das Neves',
+        zona: 'Norte',
+        bairro: 'Centro',
+        horarios: ['12:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 36,
+        name: 'Com. Filhos da Misericórdia',
+        zona: 'Norte',
+        bairro: 'Ipês',
+        horarios: ['19:30'],
+        dias: ['Terça']
+    },
+    { 
+        id: 37,
+        name: 'Paróq. Mãe dos Homens',
+        zona: 'Norte',
+        bairro: 'Centro',
+        horarios: ['16:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 38,
+        name: 'Igreja Santa Júlia',
+        zona: 'Norte',
+        bairro: 'Torre',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 39,
+        name: 'Paróq. N. S. de Aparecida',
+        zona: 'Norte',
+        bairro: '13 de Maio',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 40,
+        name: 'Paróq. Santanna e S. Joaquim',
+        zona: 'Norte',
+        bairro: 'Pedro Gondim',
+        horarios: ['18:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 41,
+        name: 'Paróq. S. Inácio de Loyola',
+        zona: 'Norte',
+        bairro: 'Mandacaru',
+        horarios: ['18:30'],
+        dias: ['Terça']
+    },
+    { 
+        id: 42,
+        name: 'Igreja São Gonaçalo',
+        zona: 'Norte',
+        bairro: 'Torre',
+        horarios: ['19:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 43,
+        name: 'Casa São Miguel - Com. Em Adoração',
+        zona: 'Sul',
+        bairro: 'Bancários',
+        horarios: ['6:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 44,
+        name: 'Paróq. São Francisco de Assis',
+        zona: 'Sul',
+        bairro: 'Bancários',
+        horarios: ['6:30'],
+        dias: ['Terça']
+    },
+    { 
+        id: 45,
+        name: 'Par. S. Antônio do Menino Deus',
+        zona: 'Sul',
+        bairro: 'Jd. Cid. Univ.',
+        horarios: ['67:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 46,
+        name: 'Paróq. Jesus Cristo Rei',
+        zona: 'Sul',
+        bairro: 'Mangabeira',
+        horarios: ['16:30'],
+        dias: ['Terça']
+    },
+    { 
+        id: 47,
+        name: 'Paróq. Santíssima Trindade',
+        zona: 'Sul',
+        bairro: 'Valentina',
+        horarios: ['17:15'],
+        dias: ['Terça']
+    },
+    { 
+        id: 48,
+        name: 'Paróq. São João Batista',
+        zona: 'Sul',
+        bairro: 'Costa e Silva',
+        horarios: ['18:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 49,
+        name: 'Paróq. de Aparecida',
+        zona: 'Sul',
+        bairro: 'Valentina',
+        horarios: ['19:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 50,
+        name: 'Doce Mãe de Deus',
+        zona: 'Sul',
+        bairro: 'Geisel',
+        horarios: ['19:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 51,
+        name: 'Paróq. S. Antônio de Pádua',
+        zona: 'Sul',
+        bairro: 'Geisel',
+        horarios: ['19:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 52,
+        name: 'Paróq. Jesus Ressuscitado',
+        zona: 'Sul',
+        bairro: 'Anatólia',
+        horarios: ['19:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 53,
+        name: 'Paróq. Santana',
+        zona: 'Sul',
+        bairro: 'Funcionários II',
+        horarios: ['19:30'],
+        dias: ['Terça']
+    },
+    { 
+        id: 54,
+        name: 'Paróq. São José',
+        zona: 'Sul',
+        bairro: 'José Américo',
+        horarios: ['19:30'],
+        dias: ['Terça']
+    },
+    { 
+        id: 55,
+        name: 'Igreja N. S. do Rosário',
+        zona: 'Oeste',
+        bairro: 'Jaguaribe',
+        horarios: ['17:00'],
+        dias: ['Terça']
+    },
+    { 
+        id: 56,
+        name: 'Paróq. São José Operário',
+        zona: 'Oeste',
+        bairro: 'Cruz das Armas',
+        horarios: ['19:00'],
+        dias: ['Terça']
+    },
+
+
+    // Quarta
+    { 
+        id: 57,
+        name: 'Sant. Imaculada Conceição',
+        zona: 'Leste | Praia',
+        bairro: 'Tambauzinho',
+        horarios: ['6:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 58,
+        name: 'Com. Doce Mãe de Deus',
+        zona: 'Leste | Praia',
+        bairro: 'Bessa',
+        horarios: ['7:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 59,
+        name: 'Com. Fraterno Amor - Oásis',
+        zona: 'Leste | Praia',
+        bairro: 'Camboinha III',
+        horarios: ['8:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 60,
+        name: 'Seminário Arquidiocesano da Paraíba',
+        zona: 'Leste | Praia',
+        bairro: 'Castelo Branco',
+        horarios: ['7:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 61,
+        name: 'Comunidade Mel de Deus',
+        zona: 'Leste | Praia',
+        bairro: 'Cabo Branco',
+        horarios: ['7:30'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 62,
+        name: 'Paróq. S. Antônio de Lisboa',
+        zona: 'Leste | Praia',
+        bairro: 'Tambaú',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 63,
+        name: 'Paróq. S. Pedro Pescador',
+        zona: 'Leste | Praia',
+        bairro: 'Manaíra',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 64,
+        name: 'Paróq. S. Pedro S. Paulo',
+        zona: 'Leste | Praia',
+        bairro: 'Brisamar',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 65,
+        name: 'Paróq. N. S. de Guadalupe',
+        zona: 'Leste | Praia',
+        bairro: 'Cabo Branco',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 66,
+        name: 'Paróq. São Miguel',
+        zona: 'Leste | Praia',
+        bairro: 'Bessa',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 67,
+        name: 'Casa da Misericórdia - Com. Em Adoração',
+        zona: 'Leste | Praia',
+        bairro: 'Bessa',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 68,
+        name: 'Paróq. N. S. de Fátima',
+        zona: 'Leste | Praia',
+        bairro: 'Mirarmar',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 69,
+        name: 'Paróq. N. S. Auxiliadora dos Cristãos',
+        zona: 'Leste | Praia',
+        bairro: 'Bessa',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 70,
+        name: 'Paróq. N. S. do Perpétuo Socorro',
+        zona: 'Leste | Praia',
+        bairro: 'Altiplano',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 71,
+        name: 'Sant. Imaculada Conceição',
+        zona: 'Leste | Praia',
+        bairro: 'Tambauzinho',
+        horarios: ['17:15'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 72,
+        name: 'Paróq. N. S. de Nazaré',
+        zona: 'Leste | Praia',
+        bairro: 'Jd Oceania',
+        horarios: ['19:30'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 73,
+        name: 'Paróq. N. S. de Nazaré',
+        zona: 'Leste | Praia',
+        bairro: 'Poço',
+        horarios: ['19:30'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 74,
+        name: 'Paróq. São Rafael',
+        zona: 'Leste | Praia',
+        bairro: 'Castelo Branco',
+        horarios: ['19:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 75,
+        name: 'Catedral N. S. das Neves',
+        zona: 'Norte',
+        bairro: 'Centro',
+        horarios: ['12:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 76,
+        name: 'Com. Filhos da Misericórdia',
+        zona: 'Norte',
+        bairro: 'Ipês',
+        horarios: ['12:30'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 77,
+        name: 'Com. Filhos da Misericórdia',
+        zona: 'Norte',
+        bairro: 'Ipês',
+        horarios: ['19:30'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 78,
+        name: 'Paróq. Mãe dos Homens',
+        zona: 'Norte',
+        bairro: 'Centro',
+        horarios: ['16:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 79,
+        name: 'Igreja Santa Júlia',
+        zona: 'Norte',
+        bairro: 'Torre',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 80,
+        name: 'Paróq. N. S. de Aparecida',
+        zona: 'Norte',
+        bairro: '13 de Maio',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 81,
+        name: 'Paróq. Santanna e S. Joaquim',
+        zona: 'Norte',
+        bairro: 'Pedro Gondim',
+        horarios: ['18:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 82,
+        name: 'Paróq. S. Inácio de Loyola',
+        zona: 'Norte',
+        bairro: 'Mandacaru',
+        horarios: ['18:30'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 83,
+        name: 'Igreja São Gonaçalo',
+        zona: 'Norte',
+        bairro: 'Torre',
+        horarios: ['19:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 84,
+        name: 'Paróq. Santa Teresinha',
+        zona: 'Norte',
+        bairro: 'Roger',
+        horarios: ['19:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 85,
+        name: 'Paróq. São Francisco de Assis',
+        zona: 'Sul',
+        bairro: 'Bancários',
+        horarios: ['6:30'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 86,
+        name: 'Par. S. Antônio do Menino Jesus',
+        zona: 'Sul',
+        bairro: 'Jd. Cid. Univ.',
+        horarios: ['7:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 87,
+        name: 'Paróq. Jesus Cristo Rei',
+        zona: 'Sul',
+        bairro: 'Mangabeira I',
+        horarios: ['16:30'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 88,
+        name: 'Paróq. Santíssima Trindade',
+        zona: 'Sul',
+        bairro: 'Valentina',
+        horarios: ['17:15'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 89,
+        name: 'Paróq. São João Batista',
+        zona: 'Sul',
+        bairro: 'Costa e Silva',
+        horarios: ['18:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 90,
+        name: 'Doce Mãe de Deus',
+        zona: 'Sul',
+        bairro: 'Geisel',
+        horarios: ['19:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 91,
+        name: 'Paróq. S. Antônio de Pádua',
+        zona: 'Sul',
+        bairro: 'Geisel',
+        horarios: ['19:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 92,
+        name: 'Paróq. Jesus Ressuscitado',
+        zona: 'Sul',
+        bairro: 'Anatólia',
+        horarios: ['19:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 93,
+        name: 'Paróq. São José',
+        zona: 'Sul',
+        bairro: 'José Américo',
+        horarios: ['19:30'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 94,
+        name: 'Igreja N. S. do Rosário',
+        zona: 'Oeste',
+        bairro: 'Jaguaribe',
+        horarios: ['17:00'],
+        dias: ['Quarta']
+    },
+    { 
+        id: 95,
+        name: 'São Judas Tadeu',
+        zona: 'Norte',
+        bairro: 'Torre',
+        horarios: ['6:00'],
+        dias: ['Quarta']
+    },
+]
